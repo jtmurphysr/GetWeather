d4383c3f087cf50efd58249305162cb904200453
jtmurphysr <john@bicycle.ninja>
2017-03-30 12:32:08 -0600

    Initial commit - repo creation

---
+++ GetWeather/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  GetWeather
+//
+//  Created by John Murphy on 3/30/17.
+//  Copyright © 2017 John Murphy. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+let BG_Color = "51A4FF"
+
